2a45361b25041715e5e5486dc2f4278755c44729
Janith Jayasuriya <45307786+janithsjay@users.noreply.github.com>
2022-09-19 20:14:51 +0530

    fixes

--- jenkinsfiles/jenkinsfile.groovy
+++ jenkinsfiles/jenkinsfile.groovy
@@ -1,12 +1,5 @@
 @Library('') _
 def parameters = [
-        systemName:         "",
-        applicationName:    "",
-        sonarQubeEnv:       "",
-        jiraIssueUpdate:    false ,
-        jiraIssuePattern:   "",
-        jiraAuth:           "",
-        jiraIssueField:     "",
         additionalDockerBuildParameters: "--build-arg applicationName=shared --no-cache --output type=local,dest=./dist",
         lambdaBuild: true
 ]
